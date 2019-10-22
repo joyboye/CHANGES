269e2e1f494f1b9659d2b6dc7cf686e0e2de2fb3
Onom "Joy" Boye <47795485+joyboye@users.noreply.github.com>
2019-10-22 12:52:38 -0500

    Add files via upload

---
+++ Edit.cshtml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NewApp1.Data;
+
+namespace NewApp1.Pages.Analytics
+{
+    public class EditModel : PageModel
+    {
+        private readonly NewApp1.Data.ApplicationDbContext _context;
+
+        public EditModel(NewApp1.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Employees Employees { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Employees = await _context.Employees.FirstOrDefaultAsync(m => m.EID == id);
+
+            if (Employees == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Employees).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeesExists(Employees.EID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool EmployeesExists(int id)
+        {
+            return _context.Employees.Any(e => e.EID == id);
+        }
+    }
+}
